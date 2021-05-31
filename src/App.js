0b6cb0178dfd9a5d130669ef84574f1f5a5b7298
Umit Turk <uyt.turk@gmail.com>
2021-06-01 01:08:58 +0300

    handleClick

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+
+const data = [
+  {
+    id: 1,
+    title: "test 1",
+  },
+  {
+    id: 2,
+    title: "Test 2",
+  },
+  {
+    id: 3,
+    title: "test 3",
+  },
+  {
+    id: 4,
+    title: "Test 4",
+  },
+];
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [result, setResult] = useState(false);
+  const searchRef = useRef();
+
+  const isTyping = search.replace(/\s+/, "").length > 0;
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (e) => {
+   if(searchRef.current && !searchRef.current.contains(e.target)){
+     setSearch('')
+   }
+  };
+
+  useEffect(() => {
+    if (isTyping) {
+      const filteredResult = data.filter((item) =>
+        item.title.toLowerCase().includes(search.toLowerCase())
+      );
+      setResult(filteredResult.length > 0 ? filteredResult : false);
+    } else {
+      setResult(false);
+    }
+
+
+    const getData = setTimeout(() => {
+      fetch(`https://restcountries.eu/rest/v2/all`)
+      .then(res => res.json())
+      .then(data => setResult(data))
+    }, 500);
+    
+
+    return () => {
+      clearTimeout(getData)
+    }
+
+  }, [search]);
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="search" ref={searchRef}>
+        <input
+          value={search}
+          type="text"
+          className={isTyping ? "typing" : null}
+          placeholder="I am looking for..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        {isTyping && (
+          <div className="search-result">
+            {result &&
+              result.map((item) => (
+                <div key={item.id} className="search-result-item">
+                  {item.title}
+                </div>
+              ))}
+            {!result && (
+              <div className="result-not-found">
+                "{search}" has not been found yet!
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
